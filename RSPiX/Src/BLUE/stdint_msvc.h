da555059574643954f931eda1c53f77de7b6a5c5
GravisZro <GravisZro@localhost>
2017-02-17 16:55:30 -0500

    Temp hack/fix until I can debug it.

--- RSPiX/Src/BLUE/stdint_msvc.h
+++ RSPiX/Src/BLUE/stdint_msvc.h
@@ -1,3 +1,40 @@
+#if 1
+#include <BaseTsd.h>
+
+typedef SSIZE_T ssize_t;
+
+#ifndef __WORDSIZE
+#define __WORDSIZE WORDSIZE
+#endif
+
+# if __WORDSIZE == 64
+#  define __INT64_C(c)	c ## L
+#  define __UINT64_C(c)	c ## UL
+# else
+#  define __INT64_C(c)	c ## LL
+#  define __UINT64_C(c)	c ## ULL
+# endif
+
+/* Limits of integral types.  */
+
+/* Minimum of signed integral types.  */
+# define INT8_MIN		(-128)
+# define INT16_MIN		(-32767-1)
+# define INT32_MIN		(-2147483647-1)
+# define INT64_MIN		(-__INT64_C(9223372036854775807)-1)
+/* Maximum of signed integral types.  */
+# define INT8_MAX		(127)
+# define INT16_MAX		(32767)
+# define INT32_MAX		(2147483647)
+# define INT64_MAX		(__INT64_C(9223372036854775807))
+
+/* Maximum of unsigned integral types.  */
+# define UINT8_MAX		(255)
+# define UINT16_MAX		(65535)
+# define UINT32_MAX		(4294967295U)
+# define UINT64_MAX		(__UINT64_C(18446744073709551615))
+#else
+
 // ISO C9x  compliant stdint.h for Microsoft Visual Studio
 // Based on ISO/IEC 9899:TC2 Committee draft (May 6, 2005) WG14/N1124
 //
@@ -245,3 +282,4 @@ typedef uint64_t  uintmax_t;
 
 
 #endif // _MSC_STDINT_H_ ]
+#endif
