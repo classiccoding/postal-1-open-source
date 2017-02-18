35e36f8a087f005c6e1e4a763316300b323f8392
GravisZro <GravisZro@localhost>
2017-02-17 21:24:05 -0500

    experimental

--- net.cpp
+++ net.cpp
@@ -18,55 +18,42 @@
 // net.cpp
 // Project: Nostril (aka Postal)
 //
-//	History:
-//		08/18/97 MJR	Created this to hold some generic stuff.
+//  History:
+//    08/18/97 MJR  Created this to hold some generic stuff.
 //
-//		08/27/97 MJR	Shortened text so it would fit in the text field.
+//    08/27/97 MJR  Shortened text so it would fit in the text field.
 //
 ////////////////////////////////////////////////////////////////////////////////
 
 #include "net.h"
 #include "RSPiX.h"
 
-#ifdef WIN32
-	#define NETNAMESPACE
-#else
-	#define NETNAMESPACE		Net::
-#endif
-
 // Lookup tables associated with the CNetLimits::NetBandwidth enums.
-#ifdef WIN32
-namespace Net
-	{
-#endif
 
-	int32_t	NETNAMESPACE lBandwidthValues[Net::NumBandwidths] =
-		{
-		   1400,						// Analog14_4
-		   2400,						// Analog28_8
-		   2400,						// Analog33_6
-		   2400,						// Analog57_6
-		   5600,						// ISDN1Channel
-		  11200,						// ISDN2Channel
-		 800000,						// LAN10Mb
-		1000000						// LAN100Mb
-		};
+int32_t Net::lBandwidthValues[Net::NumBandwidths] =
+{
+  1400,                 // Analog14_4
+  2400,                 // Analog28_8
+  2400,                 // Analog33_6
+  2400,                 // Analog57_6
+  5600,                 // ISDN1Channel
+  11200,                // ISDN2Channel
+  800000,               // LAN10Mb
+  1000000               // LAN100Mb
+};
 
-   const char* NETNAMESPACE BandwidthText[Net::NumBandwidths] =
-		{
-		"14.4 Modem",						// Analog14_4
-		"28.8 Modem",						// Analog28_8
-		"33.6 Modem",						// Analog33_6
-		"57.6 Modem",						// Analog57_6
-		"ISDN, 1 Channel",				// ISDN1Channel
-		"ISDN, 2 Channels",				// ISDN2Channel
-		"10Mb LAN (or T1)",				// LAN10Mb
-		"100Mb LAN (or T3)"				// LAN100Mb
-		};
+const char* Net::BandwidthText[Net::NumBandwidths] =
+{
+  "14.4 Modem",         // Analog14_4
+  "28.8 Modem",         // Analog28_8
+  "33.6 Modem",         // Analog33_6
+  "57.6 Modem",         // Analog57_6
+  "ISDN, 1 Channel",    // ISDN1Channel
+  "ISDN, 2 Channels",   // ISDN2Channel
+  "10Mb LAN (or T1)",   // LAN10Mb
+  "100Mb LAN (or T3)"   // LAN100Mb
+};
 
-#ifdef WIN32
-	}
-#endif
 
 
 ////////////////////////////////////////////////////////////////////////////////
