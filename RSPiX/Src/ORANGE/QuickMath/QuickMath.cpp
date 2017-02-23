3759da8cdf09bbaa922aa3765963da26baba2af7
GravisZro <GravisZro@localhost>
2017-02-22 20:47:28 -0500

    cleanup and one logic fix

--- RSPiX/Src/ORANGE/QuickMath/QuickMath.cpp
+++ RSPiX/Src/ORANGE/QuickMath/QuickMath.cpp
@@ -25,86 +25,85 @@ int16_t ATANQ[60];
 int16_t	SQRTQ[MAX_FAST_SQRT];
 
 int16_t rspATan(int16_t sDeltaY,int16_t sDeltaX)
-	{
-	int16_t sDelX,sDelY,sDeg; // absolute versions
+{
+  // absolute versions
+  int16_t sDelX = sDeltaX < 0 ? -sDeltaX : sDeltaX;
+  int16_t sDelY = sDeltaY < 0 ? -sDeltaY : sDeltaY;
+  int16_t sDeg;
 
-	if (sDeltaX < 0) sDelX = -sDeltaX;
-	else sDelX = sDeltaX;
+  if (sDelY <= sDelX)
+  {
+    if (sDelX)
+      sDeg = ATANQ[int32_t(0.5 + (rspRadToDeg * sDelY) / sDelX)];
+    else
+      sDeg = 90;
+  }
+  else
+  {
+    if (sDelY)
+      sDeg = 90 - ATANQ[int32_t(0.5 + (rspRadToDeg * sDelX) / sDelY)];
+    else
+      sDeg = 90;
+  }
 
-	if (sDeltaY < 0) sDelY = -sDeltaY;
-	else sDelY = sDeltaY;
+  // Keep in bounds
+  if (sDeltaX < 0)
+    sDeg = 180 - sDeg;
+  if (sDeltaY < 0)
+    sDeg = 360 - sDeg;
+  if (sDeg == 360)
+    sDeg = 0;
 
-	if (sDelY <= sDelX)
-		if (sDelX != 0)
-			sDeg = ATANQ[
-			int32_t(0.5 + (rspRadToDeg * sDelY) / sDelX)];
-		else
-			sDeg = 90;
-	else
-		if (sDelY) sDeg = 90 - ATANQ[
-			int32_t(0.5 + (rspRadToDeg * sDelX) / sDelY)];
-		else sDeg = 90;
-
-	// Keep in bounds
-	if (sDeltaX < 0) sDeg = 180 - sDeg;
-	if (sDeltaY < 0) sDeg = 360 - sDeg;
-	if (sDeg == 360) sDeg = 0;
-
-	return sDeg;
-	}
+  return sDeg;
+}
 
 int16_t rspATan(double dVal)
-	{
-	int16_t sDeg; // absolute versions
-	double dAbsVal;
+{
+  // absolute versions
+  double dAbsVal = dVal < 0 ? -dVal : dVal;
+  int16_t sDeg;
 
-	if (dVal < 0) dAbsVal = -dVal;
-	else dAbsVal = dVal;
+  if (dAbsVal <= 1.0)
+    sDeg = ATANQ[int32_t(0.5 + rspRadToDeg * dAbsVal)];
+  else
+    sDeg = 90 - ATANQ[int32_t(0.5 + rspRadToDeg  / dAbsVal)];
 
-	if (dAbsVal <= 1.0)
-		sDeg = ATANQ[
-		int32_t(0.5 + rspRadToDeg * dAbsVal)];
-	else
-		sDeg = 90 - ATANQ[
-			int32_t(0.5 + rspRadToDeg  / dAbsVal)];
+  // Keep in bounds
+  if (dVal < 0.0)
+    sDeg = 360 - sDeg;
+  if (sDeg == 360)
+    sDeg = 0;
 
-	// Keep in bounds
-	if (dVal < 0.0) sDeg = 360 - sDeg;
-	if (sDeg == 360) sDeg = 0;
-
-	return sDeg;
-	}
+  return sDeg;
+}
 
 void InitTrig()
-	{
-	int16_t i;
-	double rad;
+{
+  int16_t i;
+  double rad;
 
-	const double cdStepsToRad = 
-		rspPI * 2.0 / double(csNumRotSteps);
+  const double cdStepsToRad = rspPI * 2.0 / double(csNumRotSteps);
 
-	for (i=0;i<csNumRotSteps;i++)
-		{
-		rad = (double)i * cdStepsToRad;
-		SINQ[i] = (double)sin(rad);
-		COSQ[i] = (double)cos(rad);
-		fSINQ[i] = (float) SINQ[i];
-		fCOSQ[i] = (float) COSQ[i];
-		}
+  for (i = 0; i < csNumRotSteps; ++i)
+  {
+    rad = (double)i * cdStepsToRad;
+    SINQ[i] = (double)sin(rad);
+    COSQ[i] = (double)cos(rad);
+    fSINQ[i] = (float) SINQ[i];
+    fCOSQ[i] = (float) COSQ[i];
+  }
 
-	// Set up Arctan 45 degrees:
-	for (i=0;i<58;i++)
-		{
-		ATANQ[i] = int16_t(0.5 + atan( rspDegToRad * i ) *
-												rspRadToDeg);
-		}
+  // Set up Arctan 45 degrees:
+  for (i = 0; i < 58; ++i)
+  {
+    ATANQ[i] = int16_t(0.5 + atan(rspDegToRad * i) * rspRadToDeg);
+  }
 
-	int32_t l;
-	for (l=0; l < MAX_FAST_SQRT; l++)
-		{
-		SQRTQ[l] = int16_t(sqrt(double(l)));
-		}
+  for(int32_t l = 0; l < MAX_FAST_SQRT; ++l)
+  {
+    SQRTQ[l] = int16_t(sqrt(double(l)));
+  }
 
-	TRACE("QTRIG initialized!\n"); 	
-	RQuickTrigFP	dummy;
-	}
+  TRACE("QTRIG initialized!\n");
+  RQuickTrigFP	dummy;
+}
