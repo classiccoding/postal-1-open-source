33b7722f0d040677b436a08094712ae6532c72ca
GravisZro <GravisZro@localhost>
2017-03-02 09:05:16 -0500

    move complete

--- RSPiX/RSPiX.h
+++ RSPiX/RSPiX.h
@@ -18,7 +18,7 @@
 ///////////////////////////////////////////////////////////////////////////////
 //
 //	RSPiX.h
-// 
+//
 // History:
 //		07/19/96 JMI	Started.
 //
@@ -29,12 +29,93 @@
 // one, you will get a compile error.
 //
 //////////////////////////////////////////////////////////////////////////////
+//
+//	"RSPiXUnix.h"
+// 
+// History:
+//		06/01/04 RCG Initial implementation.
+//
+//////////////////////////////////////////////////////////////////////////////
+//
+// This file #includes all the headers for RSPiX*.lib.
+//
+//////////////////////////////////////////////////////////////////////////////
+
+//////////////////////////////////////////////////////////////////////////////
+//
+// Try to group by color for easy look up.  This may not always be possible,
+// but it should be b/c the any green headers that require orange headers
+// should already include them.
+//
+// This is a good place to define platform specific macros and such that don't
+// go into system.h (I'm not sure if there could be any, but...).
+//
+//////////////////////////////////////////////////////////////////////////////
 #ifndef RSPiX_H
 #define RSPiX_H
 
-#include "RSPiXUnix.h"
+//////////////////////////////////////////////////////////////////////////////
+// Blue headers.
+//////////////////////////////////////////////////////////////////////////////
+#include <BLUE/Blue.h>
+
+//////////////////////////////////////////////////////////////////////////////
+// Cyan headers.
+//////////////////////////////////////////////////////////////////////////////
+#include <CYAN/cyan.h>
+
+//////////////////////////////////////////////////////////////////////////////
+// Green headers.
+//////////////////////////////////////////////////////////////////////////////
+#include <GREEN/BLiT/BLIT.H>
+#include <GREEN/Hot/hot.h>
+#include <GREEN/Image/Image.h>
+#include <GREEN/Mix/mix.h>
+#include <GREEN/Sample/sample.h>
+#include <GREEN/Snd/snd.h>
+#include <GREEN/SndFx/SndFx.h>
+#include <GREEN/3D/user3d.h>
+#include <GREEN/BLiT/alphablit.h>
+
+//////////////////////////////////////////////////////////////////////////////
+// Orange headers.
+//////////////////////////////////////////////////////////////////////////////
+#include <ORANGE/CDT/listbase.h>
+#include <ORANGE/CDT/List.h>
+#include <ORANGE/CDT/slist.h>
+#include <ORANGE/CDT/QUEUE.H>
+#include <ORANGE/CDT/fqueue.h>
+#include <ORANGE/CDT/stack.h>
+#include <ORANGE/CDT/flist.h>
+#include <ORANGE/DirtRect/DirtRect.h>
+#include <ORANGE/GameLib/ANIMSPRT.H>
+#include <ORANGE/GameLib/Region.h>
+#include <ORANGE/GameLib/Shapes.h>
+#include <ORANGE/GameLib/SPRITE.H>
+#include <ORANGE/File/file.h>
+#include <ORANGE/IFF/iff.h>
+#include <ORANGE/GUI/dlg.h>
+#include <ORANGE/GUI/btn.h>
+#include <ORANGE/GUI/txt.h>
+#include <ORANGE/GUI/edit.h>
+#include <ORANGE/GUI/scrollbar.h>
+#include <ORANGE/GUI/ListBox.h>
+#include <ORANGE/GUI/PushBtn.h>
+#include <ORANGE/GUI/MultiBtn.h>
+#include <ORANGE/Meter/meter.h>
+#include <ORANGE/MsgBox/MsgBox.h>
+#include <ORANGE/Attribute/attribute.h>
+#include <ORANGE/ImageTools/lasso.h>
+#include <ORANGE/Laymage/laymage.h>
+#include <ORANGE/RString/rstring.h>
+#include <ORANGE/Channel/channel.h>
+#include <ORANGE/color/colormatch.h>
+#include <ORANGE/color/dithermatch.h>
+#include <ORANGE/str/str.h>
+#include <ORANGE/GUI/ProcessGui.h>
 
 #endif // RSPiX_H
+
 //////////////////////////////////////////////////////////////////////////////
 // EOF
 //////////////////////////////////////////////////////////////////////////////
