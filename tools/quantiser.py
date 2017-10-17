f5798f0ef1fce1bc852050de6c535e788403e77d
Declan Hoare <declanhoare@exemail.com.au>
2017-10-17 21:02:00 +1100

    simplified quantiser.py - faster, less buggy, no RPyC, more portable?

--- tools/quantiser.py
+++ tools/quantiser.py
@@ -14,24 +14,33 @@
 # You should have received a copy of the GNU General Public License along
 # with this program; if not, write to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
-
+#
 # Indexes a collection of images to the same 96-colour palette.
 
-import pygimp, sys
+import sys, distutils.spawn, subprocess
 
-if __name__ == "__main__":
-	if len(sys.argv) < 2:
-		sys.stderr.write("usage: " + sys.argv[0] + " IMAGES...\n")
-		sys.exit(1)
-	
-	image = pygimp.pdb.gimp_file_load(sys.argv[1], sys.argv[1])
-	for fname in sys.argv[2:]:
-		image.add_layer(pygimp.pdb.gimp_file_load_layer(image, fname))
-	
-	pygimp.pdb.gimp_image_convert_indexed(image, 0, 0, 96, False, False, "")
-	
-	for fname, layer in zip(reversed(sys.argv[1:]), image.layers):
-		image_copy = image.duplicate()
-		image_copy.resize(layer.width, layer.height)
-		pygimp.pdb.gimp_file_save(image_copy, layer, fname, fname)
+script = """
+argv = {0}
 
+image = pdb.gimp_file_load(argv[1], argv[1])
+for fname in argv[2:]:
+	image.add_layer(pdb.gimp_file_load_layer(image, fname))
+
+pdb.gimp_image_convert_indexed(image, 0, 0, 96, False, False, "")
+
+for fname, layer in zip(reversed(argv[1:]), image.layers):
+	image_copy = image.duplicate()
+	image_copy.resize(layer.width, layer.height)
+	pdb.gimp_file_save(image_copy, layer, fname, fname)
+
+pdb.gimp_quit(True)
+""".format(sys.argv)
+
+gimp_path = distutils.spawn.find_executable("gimp")
+if not gimp_path:
+	sys.stderr.write("Could not find GIMP.\n")
+	sys.exit(1)
+
+proc = subprocess.Popen([gimp_path, "-i", "--batch-interpreter=python-fu-eval", "-b", script], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+proc.wait()
+sys.exit(proc.returncode)
